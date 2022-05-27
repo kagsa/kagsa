d07e727ff902e8f2ce965deea925fda430a6e262
Kagsa <102320208+kagsa@users.noreply.github.com>
2022-05-26 17:10:29 -0700

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,6 @@
+from .lexer import run as lexer
+from .syntax_checker import run as syntax_checker
+from .parser import run as parser
+from .compiler import run as compiler
+from .methods import *
+from .built_modules import *
