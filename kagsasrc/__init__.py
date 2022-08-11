04f98bc0278b39612295871eadfcff6c128023fa
Kagsa <102320208+kagsa@users.noreply.github.com>
2022-08-11 13:32:10 -0700

    Create __init__.py

---
+++ kagsasrc/__init__.py
@@ -0,0 +1,6 @@
+from .lexer import main as lexer
+from .syntax_checker import main as syntax_checker
+from .parser import main as parser
+from .compiler import main as compiler
+from .methods import *
+from .built_modules import *
