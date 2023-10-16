774bb19325b6df25fc51043552fc37d95c6e543b
IFM321 <105885025+IFM321@users.noreply.github.com>
2023-10-16 20:23:05 +0300

    Update parse_cparen.py

--- kagsasrc/parse_cparen.py
+++ kagsasrc/parse_cparen.py
@@ -1,16 +1,15 @@
-from .parse_endl import __init__ as parse_endl
+from .parse_endl import parse_endl as parse_endl
 
-def __init__ (value,parseMemory):
+def parse_cparen(value, parseMemory):
+    parseMemory[5] = parseMemory[5].replace('|DATA-P|', '')
 
-    parseMemory[5] = parseMemory[5].replace('|DATA-P|','')
-    if value=='{':
-        parseMemory[1]=True
+    if value == '{':
+        parseMemory[1] = True
     else:
         parseMemory[1] = False
-        if parseMemory[0]==0:
-            #print(parsed_input)
-            raise SyntaxError('invalid syntax (<file>, line '+ str(parseMemory[4]) +')\nunknown place for }')
-        parseMemory[0]-=1
+        if parseMemory[0] == 0:
+            raise SyntaxError('invalid syntax (<file>, line ' + str(parseMemory[4]) + ')\nunknown place for }')
+        parseMemory[0] -= 1
     
-    parseMemory = parse_endl('\n',parseMemory,addline=False)
+    parseMemory = parse_endl('\n', parseMemory, addline=False)
     return parseMemory
