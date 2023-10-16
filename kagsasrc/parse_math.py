f2ec01a250e7c0b3f7e720f43d4aef4563b5ad38
IFM321 <105885025+IFM321@users.noreply.github.com>
2023-10-16 20:24:28 +0300

    Update parse_math.py

--- kagsasrc/parse_math.py
+++ kagsasrc/parse_math.py
@@ -1,25 +1,31 @@
+def __init__(value, parseMemory):
+    value = value.replace(' ', '')
 
-def __init__ (value,parseMemory):
-
-    value=value.replace(' ','')
-    if (parseMemory[7] == 'else'):
+    if parseMemory[7] == 'else':
         raise SyntaxError(f'invalid syntax (<file>, line {parseMemory[4]})\ncan\'t add any arguments to else command')
-    if (parseMemory[7] == 'catch'):
+
+    if parseMemory[7] == 'catch':
         raise SyntaxError(f'invalid syntax (<file>, line {parseMemory[4]})\ncan\'t add any arguments to catch command')
-    if (parseMemory[7] == 'try'):
+
+    if parseMemory[7] == 'try':
         raise SyntaxError(f'invalid syntax (<file>, line {parseMemory[4]})\ncan\'t add any arguments to try command')
-    
+
     if '|DATA-P|' in parseMemory[5]:
-        parseMemory[5]=parseMemory[5].replace('|DATA-P|','')
+        parseMemory[5] = parseMemory[5].replace('|DATA-P|', '')
+
     elif '|DATA|' in parseMemory[5]:
-        parseMemory[5]=parseMemory[5].replace('|DATA|',f'{value}|DATA|')
-    elif ('|DATA1|' in parseMemory[5]) and (parseMemory[6] == False):
-            raise SyntaxError(f'invalid syntax (<file>, line {parseMemory[4]})\nVariable opened without assign')
+        parseMemory[5] = parseMemory[5].replace('|DATA|', f'{value}|DATA|')
+
+    elif '|DATA1|' in parseMemory[5] and not parseMemory[6]:
+        raise SyntaxError(f'invalid syntax (<file>, line {parseMemory[4]})\nVariable opened without assignment')
+
     elif '|DATA1|' in parseMemory[5]:
-        parseMemory[5]=parseMemory[5].replace('|DATA1|',f'{value}|DATA1|')
-    elif parseMemory[5]=='':
-        parseMemory[5]+=f'{value}|DATA|'
+        parseMemory[5] = parseMemory[5].replace('|DATA1|', f'{value}|DATA1|')
+
+    elif parseMemory[5] == '':
+        parseMemory[5] += f'{value}|DATA|'
+
     else:
-        raise SyntaxError(f'invalid syntax (<file>, line {parseMemory[4]})\nUnknown place of math symbol')
-    
+        raise SyntaxError(f'invalid syntax (<file>, line {parseMemory[4]})\nUnknown place for math symbol')
+
     return parseMemory
