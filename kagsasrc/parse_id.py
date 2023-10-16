f8695f91d2723cefcced991f751e091c298d5798
IFM321 <105885025+IFM321@users.noreply.github.com>
2023-10-16 20:24:00 +0300

    Update parse_id.py

--- kagsasrc/parse_id.py
+++ kagsasrc/parse_id.py
@@ -1,53 +1,43 @@
-class ParseError (Exception):pass
+class ParseError(Exception):
+    pass
 
-def __init__ (value,parseMemory):
-
-    # Catch Error
-    if (parseMemory[7] == 'else'):
+def __init__(value, parseMemory):
+    if parseMemory[7] == 'else':
         raise SyntaxError(f'invalid syntax (<file>, line {parseMemory[4]})\ncan\'t add any arguments to else command')
-    if (parseMemory[7] == 'catch'):
+    if parseMemory[7] == 'catch':
         raise SyntaxError(f'invalid syntax (<file>, line {parseMemory[4]})\ncan\'t add any arguments to catch command')
-    if (parseMemory[7] == 'try'):
+    if parseMemory[7] == 'try':
         raise SyntaxError(f'invalid syntax (<file>, line {parseMemory[4]})\ncan\'t add any arguments to try command')
-    
-    # Replace All The Symbols in The Var Name : @$^~:?
-    value = value.replace('@','أ').replace('$','ب').replace('^','ت').replace('~','ث').replace('?','ج')
-    
-    # Variables that more than 1 Length :
-    # my_name   or   my_age
-    if (len(value)>2) or (len(value)==2 and not(value.startswith('#'))):
-        # Replace From This         To This
-        # my_var                    m_y___v_a_r
-        value='_'.join(list(value))
-    
-    # 4h5  =  ID
-    # #4  =  ID
-    if value[0] in '1234567890' :
-        value='_'+value
-    
-    # catch the words that used in class
-    if (value == 'أ_c_o_n_s_t_r_u_c_t_o_r')    : value='__init__' 
-    if (value == 'أ_s_t_r_i_n_g')    : value='__str__' 
-    if (value == 'أ_r_e_p_r')    : value='__repr__' 
-    if (value == 'أ_t_h_i_s')    : value='self' 
 
-    # if function name not set yet
+    value = value.replace('@', 'أ').replace('$', 'ب').replace('^', 'ت').replace('~', 'ث').replace('?', 'ج')
+
+    if len(value) > 2 or (len(value) == 2 and not value.startswith('#')):
+        value = '_'.join(list(value))
+
+    if value[0] in '1234567890':
+        value = '_' + value
+
+    if value == 'أ_c_o_n_s_t_r_u_c_t_o_r':
+        value = '__init__'
+    if value == 'أ_s_t_r_i_n_g':
+        value = '__str__'
+    if value == 'أ_r_e_p_r':
+        value = '__repr__'
+    if value == 'أ_t_h_i_s':
+        value = 'self'
+
     if '|DATA_N|' in parseMemory[5]:
-        parseMemory[5]=parseMemory[5].replace('|DATA_N|',value)
-    # if |DATA-P| not set yet
+        parseMemory[5] = parseMemory[5].replace('|DATA_N|', value)
     elif '|DATA-P|' in parseMemory[5]:
-        parseMemory[5]=parseMemory[5].replace('|DATA-P|',value)
-    # if |DATA| in line
+        parseMemory[5] = parseMemory[5].replace('|DATA-P|', value)
     elif '|DATA|' in parseMemory[5]:
-        parseMemory[5]=parseMemory[5].replace('|DATA|',f'{value}|DATA|')
-    # if var name not set yet
+        parseMemory[5] = parseMemory[5].replace('|DATA|', value + '|DATA|')
     elif '|DATA0|' in parseMemory[5]:
-        parseMemory[5]=parseMemory[5].replace('|DATA0|',value+'|DATA0|')
-    # if this is var & is named
+        parseMemory[5] = parseMemory[5].replace('|DATA0|', value + '|DATA0|')
     elif '|DATA1|' in parseMemory[5]:
-        parseMemory[5]=parseMemory[5].replace('|DATA1|',f'{value}|DATA1|')
-    elif parseMemory[5]=='':
-        parseMemory[5]+=f'{value}|DATA|'
+        parseMemory[5] = parseMemory[5].replace('|DATA1|', value + '|DATA1|')
+    elif parseMemory[5] == '':
+        parseMemory[5] += value + '|DATA|'
     else:
         raise ParseError(f'can\'t parsing (<file>, line {parseMemory[4]})')
 
